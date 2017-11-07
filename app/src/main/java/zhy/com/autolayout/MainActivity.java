33419b20a252ac3b71b2597a043f43aa2389e11e
wodewzl <273415345@qq.com>
2017-11-07 15:26:25 +0800

    测试

--- app/src/main/java/zhy/com/autolayout/MainActivity.java
+++ app/src/main/java/zhy/com/autolayout/MainActivity.java
@@ -1,7 +1,7 @@
 package zhy.com.autolayout;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -13,6 +13,9 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //dddd
+
+
     }
 
     @Override
