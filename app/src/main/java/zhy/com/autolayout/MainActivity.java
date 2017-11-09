12344d41a6204e44d2805fc7f730a192f555b666
wodewzl <273415345@qq.com>
2017-11-09 10:33:31 +0800

    //123测试

--- app/src/main/java/zhy/com/autolayout/MainActivity.java
+++ app/src/main/java/zhy/com/autolayout/MainActivity.java
@@ -13,9 +13,7 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //吴章龙中文测试
-
-
+        //123
     }
 
     @Override
